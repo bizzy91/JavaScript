0e5d90d6e39e68b645999c35560959c5769833fc
bizzy91 <bizzybak@gmail.com>
2022-05-06 23:00:22 +0900

    변수

---
+++ 02_변수.js
@@ -0,0 +1,26 @@
+// 변수, Variables
+
+// Number
+let number = 1;
+console.log(number);
+number = 2;
+console.log(number);
+// String
+let string = "Hello, ";
+console.log(string);
+string = "JavaScript!";
+console.log(string);
+// Boolean
+let boolean = true;
+console.log(boolean);
+boolean = false;
+console.log(boolean);
+// null, 변수를 선언하였고 null(없음을 의미) 값을 가지고 있는 상태
+let Null = null;
+console.log(Null);
+// undefined, 변수를 선언하였지만 값을 지정하지 않은 상태
+let Undefined;
+console.log(Undefined);
+
+// 상수, Constants (Read-Only)
+const a = 1;
